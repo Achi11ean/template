7fee2777a43a799f23d71347d25efb8930bafbed
Jonathen Whitford <paredes.jonathen@yahoo.com>
2025-08-23 21:50:36 -0400

    initial

--- vite-live/src/App.jsx
+++ vite-live/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage";
+import HomePage from "./Homepage";
 import About from "./About";
 import NavBar from "./NavBar";
 import Services from "./Services";
